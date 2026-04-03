c93b06dc60b757fd0bf506b25f12adf3de588768
brian pagsolingan <brian.pagsolingan@gmail.com>
2026-04-03 16:02:42 -0400

    connected ui elements to viewcontroller

--- A2_iOS_BRIAN_101490258/A2_IOS_brian_101490258/A2_IOS_brian_101490258/ViewController.swift
+++ A2_iOS_BRIAN_101490258/A2_IOS_brian_101490258/A2_IOS_brian_101490258/ViewController.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var lblID: UILabel!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblDesc: UILabel!
+    @IBOutlet weak var lblPrice: UILabel!
+    @IBOutlet weak var lblProvider: UILabel!
+    @IBOutlet weak var btnPrev: UIButton!
+    @IBOutlet weak var btnNext: UIButton!
+    
+    var products: [NSManagedObject] = []
+    var currentIndex = 0
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Product Viewer"
+        searchBar.delegate = self
+        fetchProducts()
     }
+    
+    
 
 
 }
